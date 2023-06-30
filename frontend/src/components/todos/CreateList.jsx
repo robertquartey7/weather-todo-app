7a889d0239f1ba1d7549ca97dbfb06635843a9b9
Robert Quartey <rquartey_ifwd@TKH-M1-Macbook-Air-10.local>
2023-06-30 15:33:18 -0400

    .

--- frontend/src/components/todos/CreateList.jsx
+++ frontend/src/components/todos/CreateList.jsx
@@ -6,7 +6,7 @@ import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
 function CreateList() {
   const dataRef = useRef();
-  const navigate = useNavigatevigate();
+  const navigate = useNavigate();
   const {
     register,
     handleSubmit,
